80b75f343ba7813fda5c9131704cbec803047a55
Marco Valtas <mavcunha@gmail.com>
2022-08-21 21:03:10 -0400

    Adds fallback to 'default' key

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,18 +1,20 @@
 from parse_config import parse
 
-
 def valid_key(path):
     # will consider a valid path if has no slashes and is not None
     return path is not None and not path.count('/') and path.isalnum()
 
-
 def clean_path(path):
     return path.strip('/') if path else None
 
-
 def application(environ, start_response):
-    path = clean_path(environ.get('PATH_INFO', ''))
+    path = clean_path(environ.get('PATH_INFO', 'default'))
     shorts = parse('shorturls.cfg')
+
+    # if not path was given, let's fallback to 'default'
+    # as key. If that is not present, than 404
+    if not path:
+        path = 'default'
 
     if valid_key(path) and path in shorts:
         start_response('303 See Other', [('Location', shorts[path])])
--- test_passenger_wsgi.py
+++ test_passenger_wsgi.py
@@ -4,15 +4,23 @@ from mock import patch, Mock
 import passenger_wsgi
 
 
-@patch("passenger_wsgi.parse", return_value={'key': 'value'})
+@patch("passenger_wsgi.parse", return_value={'key': 'value', 'default': 'default_response'})
 class PassengerWsgiTestCase(unittest.TestCase):
 
     def setUp(self):
         self.response = Mock()
 
-    def test_empty_path_info_does_not_redirect(self, parse):
+    def test_redirect_to_default_if_path_is_empty(self, parse):
         passenger_wsgi.application({'PATH_INFO': ''}, self.response)
-        self.response.assert_called_with('404 Not Found', [('Content-type', 'text/plain')])
+        self.response.assert_called_with('303 See Other', [('Location', 'default_response')])
+
+    def test_redirect_to_default_if_path_is_missing(self, parse):
+        passenger_wsgi.application({}, self.response)
+        self.response.assert_called_with('303 See Other', [('Location', 'default_response')])
+
+    def test_redirect_to_default_if_path_is_default(self, parse):
+        passenger_wsgi.application({'PATH_INFO': 'default'}, self.response)
+        self.response.assert_called_with('303 See Other', [('Location', 'default_response')])
 
     def test_valid_path_returns_false_for_invalid(self, parse):
         paths = ['/some/path', '/ ', '/  ', '/$#@', '/?this=that', '   ']
@@ -29,10 +37,6 @@ class PassengerWsgiTestCase(unittest.TestCase):
 
     def test_not_redirect_if_not_found(self, parse):
         passenger_wsgi.application({'PATH_INFO': '/non-existent'}, self.response)
-        self.response.assert_called_with('404 Not Found', [('Content-type', 'text/plain')])
-
-    def test_not_redirect_if_path_info_missing(self, parse):
-        passenger_wsgi.application({}, self.response)
         self.response.assert_called_with('404 Not Found', [('Content-type', 'text/plain')])
 
     def test_redirects_if_key_found(self, parse):
